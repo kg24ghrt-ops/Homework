a1ffcdda74641b487faf4e74a8026db890a5601c
Ko Ko <your-email@example.com>
2026-02-20 07:18:26 +0630

    CI: Add auto-update step for Gradle Wrapper to fix build errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    // This version 8.2.2 matches the Gradle 8.2 requirement
     id("com.android.application") version "8.2.2" apply false
     id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false // Ensure this is 1.9.0
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
